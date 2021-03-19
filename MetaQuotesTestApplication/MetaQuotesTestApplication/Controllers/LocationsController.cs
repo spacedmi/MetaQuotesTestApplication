6d8f10bccc455ddde72f3b69f631f946655b058b
spacedmi <spacedmi@gmail.com>
2021-03-19 23:19:00 +0300

    controller and db file

---
+++ MetaQuotesTestApplication/MetaQuotesTestApplication/Controllers/LocationsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MetaQuotes.TestApplication.Controllers
+{
+    [ApiController]
+    public class LocationsController : ControllerBase
+    {
+        public LocationsController()
+        {
+
+        }
+
+        [HttpGet("/ip/location")]
+        public ActionResult<string> GetIpLocation(string ip)
+        {
+            return ip;
+        }
+
+
+        [HttpGet("/city/locations")]
+        public ActionResult<string> GetCityLocations(string city)
+        {
+            return city;
+        }
+    }
+}
